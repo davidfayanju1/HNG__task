a526e149ef3945710dbe936fd7df80ca9308a5b8
davidfayanju1 <bayo@gmail.com>
2022-11-04 00:09:43 +0100

    basic styling for the contact page

---
+++ src/pages/Contact.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../styles/Contact.module.css';
+
+const Contact = () => {
+  return (
+    <div className='container'>
+        <h1>Contact me</h1>
+    </div>
+  )
+}
+
+export default Contact
