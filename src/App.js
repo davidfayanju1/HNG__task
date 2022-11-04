075a13f10c7fd82b4e93ddc539afda72b044871f
davidfayanju1 <bayo@gmail.com>
2022-11-04 01:37:33 +0100

    added styling to the mark up pages

--- src/App.js
+++ src/App.js
@@ -2,15 +2,19 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
 import Contact from './pages/Contact';
+import Footer from './components/Footer';
 
 function App() {
 
 
   return (
-    <Routes>
-      <Route path='/' element={ <Home />} />
-      <Route path='/contact' element={ <Contact />} />
-    </Routes>
+    <>
+      <Routes>
+        <Route path='/' element={ <Home />} />
+        <Route path='/contact' element={ <Contact />} />
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,10 +1,45 @@
 import React from 'react'
-import '../styles/Contact.module.css';
+import '../styles/Contact.css';
 
 const Contact = () => {
   return (
     <div className='container'>
-        <h1>Contact me</h1>
+        <div className="title">
+            <h1>Contact Me</h1>
+            <p>Hi there, contact me to ask  me about anything you have in mind.</p>
+        </div>
+
+        <form>
+            <div className='form-flex'>
+                <div className="form-group">
+                    <label htmlFor="first-name">First Name</label>
+                    <input type="text" placeholder='Enter your first name'/>
+                </div>
+                <div className="form-group">
+                    <label htmlFor="last-name">Last Name</label>
+                    <input type="text" placeholder='Enter your last name'/>
+                </div>
+            </div>
+
+            <div className="form-group">
+                <label htmlFor="first-name">Email</label>
+                <input type="email" placeholder='yourname@email.com'/>
+            </div>
+
+            <div className="form-group" id='messageText'>
+                <label htmlFor="message">Message</label>
+                <textarea name="message" id="message" cols="30" rows="10" placeholder="Send me a message and I'll reply you as soon as possible"></textarea>
+            </div>
+
+            <div className="form-check">
+                <input type="checkbox" name="" id="" />
+                <small>You agree to providing your data (name) who may contact you.</small>
+            </div>
+
+            <div className="form-group">
+                <input type="submit" value="Send message" />
+            </div>
+        </form>
     </div>
   )
 }
